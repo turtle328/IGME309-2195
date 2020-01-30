247bcae3e90eaa0abf32507ae6af43b428e76536
Alex Rosenbach <ajrosenbach@yahoo.com>
2020-01-29 20:12:20 -0500

    shaders exercise

--- _Binary/Shaders/BasicColorCompliment.fs
+++ _Binary/Shaders/BasicColorCompliment.fs
@@ -3,25 +3,25 @@
 in vec3 Color;
 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
-uniform bool Complimentary = false;
+uniform bool compOn = false;
 
 out vec4 Fragment;
 
 void main()
 {
-	vec3 output = Color;
+	vec3 currColor = Color;
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 	{
-		output = SolidColor;
+		currColor = SolidColor;
 	}
-	
-	if(Complimentary == true)
-	{
-		output = vec3(1.0f, 1.0f, 1.0f) - output;
-	}
-	
-	Fragment = vec4(output,1);
 		
+	if (compOn == true)
+	{
+		currColor = vec3(1.0f, 1.0f, 1.0f) - currColor;
+	}
+
+	Fragment = vec4(currColor, 1);
+	
 	return;
 }
